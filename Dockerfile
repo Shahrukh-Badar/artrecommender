6211777c93b1ac41e61eeb1f5e6728e0e6680b5a
Shah <shah.r.bdr@gmail.com>
2021-05-15 04:21:27 +0200

    Code Refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,22 @@
 FROM python:3.9.4-buster
 
-# copy the dependencies file to the working directory
 COPY requirements.txt /
 
-# install dependencies
 RUN pip install -r requirements.txt
 
-# Copy source code
 COPY . /
+
 COPY entrypoint.sh /entrypoint.sh
 
 RUN chmod +x /entrypoint.sh
 
-
 ENTRYPOINT ["/entrypoint.sh"]
-# Run the application
-#CMD ["python", "main.py"]
+
 
 
 #   pip freeze > requirements.txt
 #   docker build -t artrecommenderdocker .
 #   docker run artrecommenderdocker
-#   docker  exec -it artrecommenderdocker /bin/bash
+#   docker  run -it artrecommenderdocker /bin/bash
+#   docker system prune
+#   docker rmi $(docker images --filter “dangling=true” -q --no-trunc)
--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 from text_processor import *
+from Extractor import Extractor
+
+def get_statics():
+    columns_to_read = {'Dimensions': 'str'}
+    df = pd.read_csv('openaccess/MetObjects.csv', encoding='utf8', usecols=columns_to_read.keys(),
+                     dtype=columns_to_read)
+    number_of_total_rows = df.shape[0]
+    number_of_null_dimensions = df[df['Dimensions'].isnull()].shape[0]
+    df['Dimensions'] = df['Dimensions'].fillna('dimensionsunavailable')
+    df['Dimension_pattern'] = df['Dimensions'].apply(lambda x: TextProcessor.dimension_transformation(x))
+
+    df_grp = df.groupby('Dimension_pattern').first().reset_index()
+    dff = df.groupby(['Dimension_pattern']).count().reset_index()
+    dff = dff.rename(columns={'Dimensions': 'count'})
+    dff = dff.sort_values(by='count', ascending=False)
+    dff = pd.merge(dff, df_grp, on='Dimension_pattern')
+    number_of_distinct_patterns = dff[dff['Dimension_pattern'] != 'dimensionsunavailable'].shape[0]
+
+    df = dff
+    del dff
+    df['Dimension_clean'] = df['Dimensions'].apply(lambda x: TextProcessor.string_cleaning(x))
+    df['Dimension_processed'] = df.apply(lambda x: Extractor.extract_dimension_in_cm(x['Dimension_clean']), axis=1)
+    count_processed_patterns = df.loc[(df['Dimension_processed'] == 'not_processed')].shape[0]
+    count_processed_patterns_no_cm = df.loc[
+        (df['Dimension_processed'] == 'not_processed') & (~df["Dimension_clean"].str.contains('cm', na=False))].shape[0]
+
+    s = 1
 
 
 def get_statics_by_occurrence(df, new_col='NewDim'):
@@ -10,133 +37,6 @@ def get_statics_by_occurrence(df, new_col='NewDim'):
     dff.to_csv('occurrence.csv', encoding='utf8')
     return dff
 
-
-def data_preprocessing():
-    # df = pd.read_csv('MetObjects.csv',nrows=1000)
-    columns_to_read = {'Dimensions': 'str'}
-    df = pd.read_csv('MetObjects.csv', encoding='utf8', usecols=columns_to_read.keys(), dtype=columns_to_read)
-    # df = df.loc[df["Dimensions"].str.contains('approx', na=False)]
-    df['NewDim'] = df.apply(lambda x: TextProcessor.dimention_transfomation(x['Dimensions']), axis=1)
-    df['Dimensions'] = df['Dimensions'].fillna('dimensionsunavailable')
-    df['Dimensions'] = df['Dimensions'].apply(lambda x: TextProcessor.string_cleaning(x))
-    df_s = get_statics_by_occurrence(df.copy())
-    s = 1
-
-
-def get_distinct_dimensions(df):
-    x = df.Dimensions.unique()
-    df = pd.DataFrame(data=x, columns=['Dimensions'])
-    df = pd.to_csv('all_dis_dim.csv', encoding='utf8')
-    return df
-
-
-def extract_dimension_in_cm(dim):
-    result = 'not_processed'
-    if dim.lower() == 'dimensionsunavailable':
-        return result
-    if not 'cm' in dim:
-        return result
-    if ';' in dim:
-        s = 2
-
-    cm_str_orig = dim
-    cm_str = TextProcessor.replace_non_numeric_brackets(dim)
-    bracket_data = TextProcessor.extract_all_brackets_with_data(cm_str, [])
-    bracket_count = len(bracket_data)  # extract_bracket_count(cm_str)
-
-    if bracket_count == 0:
-        found = 'not_processed'
-        if 'inches' in cm_str:
-            cm_data = [x for x in cm_str.split('inches') if 'cm' in x]
-            if cm_data:
-                found = TextProcessor.extract_only_valid_dimension(cm_data[0])
-                result = found
-    elif bracket_count >= 1:
-        if bracket_count in [1, 2, 3] and all(['x' not in x for x in bracket_data]) \
-                and all(TextProcessor.extract_regex_pattern_1d(x) != 'not_processed' for x in bracket_data):
-            cm_data = 'x'.join(TextProcessor.extract_regex_pattern_1d(x) for x in bracket_data)
-            result = cm_data
-        else:
-            cm_data = [TextProcessor.extract_cm_inside_single_bracket(x) for x in bracket_data if 'cm' in x]
-            result = cm_data[0] if cm_data else 'not_processed'  # get only first if multiple pairs exists
-
-    if result == 'not_processed':  # mostly for more than 3 brackets select only first and ambiguios data 'diam:31/4in.(8.3cm)mount:201/2x15x7/8in.(52.1x38.1x2.2cm)'
-        result = TextProcessor.extract_only_valid_dimension(cm_str)
-        if result == 'not_processed':
-            s = ""
-    return result
-
-
-def pre_process():
-    columns_to_read = {'Dimensions': 'str', 'count': 'int', 'NewDim': 'str'}
-    df = pd.read_csv('occurrence.csv', encoding='utf8', usecols=columns_to_read.keys(), dtype=columns_to_read)
-
-    df['dim_clean'] = df.apply(lambda x: extract_dimension_in_cm(x['Dimensions']), axis=1)
-    df_np = df[df['dim_clean'] == 'not_processed']
-    df_np = df.loc[(df['dim_clean'] == 'not_processed') & (df["NewDim"].str.contains('cm', na=False))]
-    df_none = df.loc[(df['dim_clean'].isnull())]  # where cm is outside of bracket
-    s = 2
-
-
-def pre_process_original_data():
-    regenerate = False
-
-    if regenerate:
-        columns_to_read = {'Object ID': 'int', 'Dimensions': 'str'}
-        df = pd.read_csv('MetObjects.csv', encoding='utf8', usecols=columns_to_read.keys(), dtype=columns_to_read)
-        df['Dim_pattern'] = df['Dimensions'].apply(lambda x: TextProcessor.dimention_transfomation(x))
-        df = get_statics_by_occurrence(df, 'Dim_pattern')
-        # df.to_csv('intermediate.csv', encoding='utf8', index=True)
-        # df = pd.read_csv('intermediate.csv', encoding='utf8')
-        df['Dim_clean'] = df['Dimensions'].apply(lambda x: TextProcessor.string_cleaning(x))
-        df['Dim_processed'] = df.apply(lambda x: extract_dimension_in_cm(x['Dim_clean']), axis=1)
-
-        # d f.to_csv('processed.csv', encoding='utf8', index=True)
-    elif 1 == 12:
-        columns_to_read = {'Object ID': 'int', 'Dim_processed': 'str'}
-        df = pd.read_csv('processed.csv', encoding='utf8', usecols=columns_to_read.keys(), dtype=columns_to_read)
-        df = pd.Series(data=df['Dim_processed'], index=df['Object ID'])
-        s = 2
-    else:
-        columns_to_read = {'Object ID': 'int', 'Dimensions': 'str'}
-        df = pd.read_csv('MetObjects.csv', encoding='utf8', usecols=columns_to_read.keys(), dtype=columns_to_read)
-        df['Dim_clean'] = df['Dimensions'].apply(lambda x: TextProcessor.string_cleaning(x))
-        df['Dim_processed'] = df.apply(lambda x: extract_dimension_in_cm(x['Dim_clean']), axis=1)
-        df = df.sort_values(by=['Object ID'], ascending=True)
-        df.to_csv('processed.csv', encoding='utf8', index=True)
-    d = 2
-
-
-# df['Dimensions'] = df['Dimensions'].fillna('dimensionsunavailable')
-# df = df.loc[(df["Dim_clean"].str.contains('image', na=False))]
-# del df['Dim_processed']
-# df = df.loc[(~df['Dimensions'].str.contains('cm', na=False))]
-# df['Dim_clean'] = df['Dimensions'].apply(lambda x: string_cleaning(x))
-# df['Dim_processed'] = df.apply(lambda x: extract_dimension_in_cm(x['Dim_clean']), axis=1)
-# approximately 14.3 x 9.9 cm (5 5/8 x 3 7/8 in.)
-# l. 4 3/4 x w. 6 3/4 inches (approx.) 12.1 x 17.1 cm
-# mounts approximately: 8.9 x 17.8 cm (3 1/2 x 7 in.)
-# approx. each: image: 4 1/4 x 7 1/8 in. (10.8 x 18.1 cm) mount: 10 in. x 11 15/16 in. (25.4 x 30.4 cm)
-# s = 'approximately 14.3 x 9.9 cm (5 5/8 x 3 7/8 in.)'
-# if 'cm' in s and extract_bracket_count(s) == 1 and \
-#         ';' not in s and 'cm' not in extract_bracket(s):
-#     cm_data = [x for x in s.split(extract_bracket(s)) if 'cm' in x][0]
-#
-#     re.search('\d*\.?\d*x{1}\d*\.?\d*', s).group(0)
-#
-# s = 3
-# data_preprocessing()
-# pre_process()
-# pre_process_original_data()
-def is_fit(object_id):
-    cache_data = pd.read_pickle('final.pkl')
-    try:
-        dimension = cache_data[object_id]
-        print(dimension)
-        return False if dimension == 'not_processed' else True
-    except KeyError:
-        print('Object Id not found.')
-        return False
 
 
 class ProcessDimension:
@@ -158,7 +58,7 @@ class ProcessDimension:
         del cls.raw_data
         df['Dimensions'] = df['Dimensions'].fillna('dimensionsunavailable')
         df['Dimensions'] = df['Dimensions'].apply(lambda x: TextProcessor.string_cleaning(x))
-        df['Dimensions'] = df.apply(lambda x: extract_dimension_in_cm(x['Dimensions']), axis=1)
+        df['Dimensions'] = df.apply(lambda x: Extractor.extract_dimension_in_cm(x['Dimensions']), axis=1)
         df = df.sort_values(by=['Object ID'], ascending=True)
         df = pd.Series(data=df['Dimensions'], index=df['Object ID'])
         cls.raw_data = df
@@ -196,18 +96,24 @@ class ProcessDimension:
         except KeyError:
             print('Object Id not found.')
             return False
+        except ValueError:
+            print('Object Id must be numeric.')
+            return False
 
 
-if __name__ == "__main__":
-    process_dimension = ProcessDimension()
-    while True:
-        print('Please enter object id.')
-        object_id = input()  # 1000
-        print('Please enter dimension.')
-        dimension = input()  # '135.6x186.4x46.7'
-        if dimension:
-            print(process_dimension.does_it_fit(object_id, dimension))
-            print('Please enter q to quit and enter to continue.')
-            stop = input()
-            if stop.strip() == 'q':
-                break
+# if __name__ == "__main__":
+#     process_dimension = ProcessDimension()
+#     while True:
+#         print('Please enter object id.')
+#         object_id = input()  # 1000
+#         print('Please enter dimension.')
+#         dimension = input()  # '135.6x186.4x46.7'
+#         if dimension:
+#             print(process_dimension.does_it_fit(object_id, dimension))
+#             print('Please enter q to quit and enter to continue.')
+#             stop = input()
+#             if stop.strip() == 'q':
+#                 break
+
+
+get_statics()
